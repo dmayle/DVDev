9196c5475a899e89d66dc32afa13dcea217190fd
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-04-03 09:50:19 -0400

    Added related issues, and other issue links

--- dvdev/controllers/dv.py
+++ dvdev/controllers/dv.py
@@ -9,7 +9,9 @@ from pygments.lexers import DiffLexer
 from pygments.formatters import HtmlFormatter
 from mercurial import commands, ui, hg, patch, cmdutil
 from re import compile
+from os import path
 from filesafe import Chroot
+import yamltrak
 
 # Regex to pull the filename out of the diff header
 DIFF_FILE = compile(r'diff -r [0-91-f]+ (.*)$')
@@ -32,13 +34,25 @@ class DvController(BaseController):
             node1, node2 = cmdutil.revpair(repo, None)
 
             match = cmdutil.match(repo, (), {})
+            repodiffs = []
             for diff in patch.diff(repo, node1, node2, match=match, opts=patch.diffopts(self.ui)):
                 diffheader = diff.split('\n')[0]
                 filename = DIFF_FILE.match(diffheader).groups()[0]
                 # Should I instantiate a single lexer and formatter and share them?
-                diffs.append({'repository':root,
+                repodiffs.append({'repository':root,
                                'filename':filename,
                                'diff': highlight(diff, DiffLexer(), HtmlFormatter())})
+            # At the repo level, we want to go through all found files and look
+            # for related issues
+            issues = yamltrak.issues([repo.root])[root]
+            for diff in repodiffs:
+                relatedissues = yamltrak.relatedissues(repo.root, filename=diff['filename'], ids=issues.keys())
+                related = {}
+                for issue in relatedissues:
+                    related[issue] = {'repo':root,
+                                      'title':issues[issue]['title']}
+                diff['relatedissues'] = related
+            diffs += repodiffs
             
         c.css = '<style type="text/css">%s</style>' % HtmlFormatter().get_style_defs('.highlight')
         c.diffs = diffs
@@ -49,7 +63,11 @@ class DvController(BaseController):
         # The variable passing schema we use means that we'll have problems
         # with a repository named 'on'.  We should look into a fix for that.
         url = request.environ['routes.url']
-        io = request.params
+
+        # We send out a form with two inputs per file, one hidden with the
+        # repository, and one checkbox.  We get back two values for the file if
+        # the checkbox is checked, and one otherwise.  It's not perfect, but it
+        # works for now.
         changeset = {}
         for key in request.params:
             input = request.params.getall(key)
@@ -71,9 +89,46 @@ class DvController(BaseController):
                 continue
             repochroot = Chroot(repo.root)
             try:
-                files = (repochroot('./' + file) for file in changeset[root])
+                files = (repochroot(path.join(repo.root, file)) for file in changeset[root])
             except IOError:
                 error = 'Bad Filename'
                 redirect(url.current(action='index', error=error))
             commands.commit(self.ui, repo, message=message, logfile=None, *files)
+        redirect(url.current(action='index'))
+
+    def revert(self):
+        return
+        """Revert the given file to its pristine state."""
+        # The variable passing schema we use means that we'll have problems
+        # with a repository named 'on'.  We should look into a fix for that.
+        # TODO Make this get require a post and spit back a confirmation form
+        url = request.environ['routes.url']
+
+        repo = request.params['repo']
+        filename = request.params['filename']
+
+        if not repo or not filename:
+            redirect(url.current(action='index'))
+
+        found = False
+        for repoobj, root in self.repositories:
+            if root == repo:
+                found = True
+                repo = repoobj
+                break
+        if not found:
+            redirect(url.current(action='index'))
+        repochroot = Chroot(repo.root)
+        try:
+            filepath = repochroot('./' + filename)
+        except IOError:
+            error = 'Bad Filename'
+            redirect(url.current(action='index', error=error))
+        # repo.status() returns a tuple of lists, each lists containing the
+        # files containing a given status.  Those are:
+        # modified, added, removed, deleted, unknown, ignored, clean
+        modified, added, removed, deleted, unknown, ignored, clean = repo.status()
+        for status in [modified, added, removed, deleted]:
+            if filename in status:
+                commands.revert(self.ui, repo, filepath, rev='tip', date=None)
         redirect(url.current(action='index'))
