90756a2c423b9d92145fc5c886d496b2c9df4eb3
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-04-04 18:40:14 -0400

    Forgot to add the template. Planning tests as well

--- dvdev/tests/functional/test_wiki.py
+++ dvdev/tests/functional/test_wiki.py
@@ -5,3 +5,7 @@ class TestWikiController(TestController):
     def test_index(self):
         response = self.app.get(url(controller='wiki', action='index'))
         # Test response...
+
+    def test_view(self):
+        response = self.app.get(url(controller='wiki', action='view'))
+        # Test response...
