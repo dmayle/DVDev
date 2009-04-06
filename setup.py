f0e7cf786df31a58627eaff0e8e0625ba319ab44
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-04-06 14:22:59 -0400

    Clarify the GNU Affero GPLv3 licensing terms.

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+#   DVDev - Distributed Versioned Development - tools for Open Source Software
+#   Copyright (C) 2009  Douglas Mayle
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -9,9 +25,10 @@ setup(
     name='DVDev',
     version='0.1.2',
     description='',
-    author='',
-    author_email='',
-    url='',
+    author='Douglas Mayle',
+    author_email='douglas@mayle.org',
+    license='GNU Affero General Public License v3',
+    url='http://dvdev.org',
     install_requires=[
         "Pylons>=0.9.7",
         "Genshi>=0.4",
@@ -43,4 +60,6 @@ setup(
         [console_scripts]
         dvdev=dvdev.commands:main
     """,
+    classifiers=['License :: OSI Approved :: GNU Affero General Public License v3'
+    ]
 )
